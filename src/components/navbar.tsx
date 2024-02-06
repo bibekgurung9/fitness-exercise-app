07ddb93f1c92be0c1271c1b1a2ab64810a879419
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2024-02-06 22:13:19 +0545

    css change

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,12 @@ import { ModeToggle } from "./mode-toggle";
 
 export default function Navbar(){
   return(
-    <nav className="bg-black flex h-[64px] justify-between items-center w-full">
+    <nav className="bg-slate-950 flex h-[64px] justify-between items-center w-full border-b-4">
         <Link href="/" className="m-4 flex gap-2 text-orange-400 font-bold text-xl">
           <Dumbbell />
           <span>Fitness Nepal</span>
         </Link>  
-      <div className="hidden md:flex mr-4">
+      <div className="hidden md:flex mr-4 items-center gap-x-2">
         <ModeToggle />
         {navLinks.map((links) => (
           <div key={links.label}>
